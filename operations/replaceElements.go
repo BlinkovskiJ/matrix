9fbde0ca53a3d6d3ef27285ed38a7a7354b46251
Tangetz Roger <flipsnxmdinbbpu@gmail.com>
2024-05-13 01:37:36 +1000

    2

--- operations/replaceElements.go
+++ operations/replaceElements.go
@@ -19,3 +19,17 @@ func replaceRow(matrix [][]int, originRow int, destRow int) {
 		matrix[originRow][i] = temp
 	}
 }
+
+func transpose(matrix [][]int) {
+	transposedMatrix := make([][]int, len(matrix))
+	for i := range matrix {
+		for j := range matrix[i] {
+			transposedMatrix[i] = append(transposedMatrix[i], matrix[j][i])
+		}
+	}
+	for i := range matrix {
+		for j := range matrix[i] {
+			matrix[i][j] = transposedMatrix[i][j]
+		}
+	}
+}
