6a983736ba071449fec499c1894b5a0e60a51888
Tangetz Roger <flipsnxmdinbbpu@gmail.com>
2024-05-13 01:29:34 +1000

    1

--- operations/replaceElements.go
+++ operations/replaceElements.go
@@ -1,5 +1,15 @@
 package operations
 
+func replaceCol(matrix [][]int, originCol int, destCol int) {
+	originCol--
+	destCol--
+	for i := range matrix {
+		temp := matrix[i][destCol]
+		matrix[i][destCol] = matrix[i][originCol]
+		matrix[i][originCol] = temp
+	}
+}
+
 func replaceRow(matrix [][]int, originRow int, destRow int) {
 	originRow--
 	destRow--
