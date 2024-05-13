03f944bde984faba6f8b18733ac448372003d94e
Tangetz Roger <flipsnxmdinbbpu@gmail.com>
2024-05-13 23:55:38 +1000

    5

--- matrix.go
+++ matrix.go
@@ -16,3 +16,15 @@ func createMatrix(size int) [][]int {
 	}
 	return matrix
 }
+
+func createLimitedMatrix(size, maxValue int) [][]int {
+	rand.Seed(time.Now().UnixNano())
+	matrix := make([][]int, size)
+	for i := range matrix {
+		for j := 0; j < size; j++ {
+			randomNumber := rand.Intn(maxValue) + 1
+			matrix[i] = append(matrix[i], randomNumber)
+		}
+	}
+	return matrix
+}
